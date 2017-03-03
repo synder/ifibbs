11d3838e6d7e6dfffa177abb839787c4051dfd32
synder <812256717@qq.com>
2017-03-03 10:42:23 +0800

    no message

--- api/controller/common/captcha.js
+++ api/controller/common/captcha.js
@@ -11,13 +11,19 @@ const captchaModel = require('../../../public/model/captcha');
  * @desc 发送短信验证码
  * */
 exports.getSmsSecurityCode = function (req, res, next) {
-    let phoneNumber = req.query.phone;
+    let mobileNumber = req.query.mobile;
+    
+    if(!mobileNumber){
+        return next(new BadRequestError('mobile is need'));
+    }
+    
+    let regex = /^1[0-9]{10}$/;
 
-    if(!/^1[0-9]{10}$/.test(phoneNumber)){
-        return next(new BadRequestError('phone is illegal'));
+    if(regex.test(mobileNumber.toString()) !== true){
+        return next(new BadRequestError('mobile is illegal'));
     }
 
-    captchaModel.sendSmsSecurityCode(phoneNumber, function (err, captchaID) {
+    captchaModel.sendSmsSecurityCode(mobileNumber, function (err, captchaID) {
         if(err){
             return next(err);
         }
