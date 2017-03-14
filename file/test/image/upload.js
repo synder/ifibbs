0de2c8cf29dbe5c067b67d4f8f0fac2a92edbd71
synder <812256717@qq.com>
2017-03-14 14:38:13 +0800

    no message

--- file/test/image/upload.js
+++ file/test/image/upload.js
@@ -29,6 +29,11 @@ describe('上传接口测试', function() {
                 chai.expect(res.body).to.have.property('msg', '');
 
                 chai.expect(res.body).to.have.ownProperty('result');
+                
+                chai.expect(res.body.result).to.have.all.keys([
+                    '3c9f2d55454e007bc0e43a193f37f9e1.jpg', 
+                    'eac4644bdb3d24ba8feabfa860777622.jpg'
+                ]);
 
                 done();
             });
