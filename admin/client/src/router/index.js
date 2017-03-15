10368fd7b7093ba14ed7be23094b06f2053cfeae
jk <914158992@qq.com>
2017-03-15 18:12:47 +0800

    no message

--- admin/client/src/router/index.js
+++ admin/client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Header from '@/components/header'
+import Navbar from '@/components/navbar'
 
 Vue.use(Router)
 
@@ -9,13 +10,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+
     },
     {
       path: '/header',
       name: 'Header',
       component: Header
-    }
+    },
   ]
 })
